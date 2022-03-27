630474b27aaf98123fedae78d1aa1e02605bde03
Matd127 <79983536+Matd127@users.noreply.github.com>
2022-03-27 18:39:27 +0200

    Copier.cs

--- Zadanie1/Copier.cs
+++ Zadanie1/Copier.cs
@@ -8,14 +8,56 @@ namespace Zadanie1
 {
     public class Copier : BaseDevice, IPrinter, IScanner
     {
-        public void Print(in IDocument document)
+        public int PrintCounter { get; set; }
+        public int ScanCounter { get; set; }
+        public new int Counter { get; set; }
+
+        public new void PowerOff()
         {
-            throw new NotImplementedException();
+            if (state != IDevice.State.off) state = IDevice.State.off;
         }
 
-        public void Scan(out IDocument document, IDocument.FormatType formatType)
+        public new void PowerOn()
         {
-            throw new NotImplementedException();
+            if (state != IDevice.State.on)
+            {
+                state = IDevice.State.on;
+                Counter++;
+            }
+        }
+
+        public void Print(in IDocument document)
+        {
+            if (state == IDevice.State.off) return;
+            PrintCounter++;
+            Console.WriteLine(DateTime.Now + " Print: " + document.GetFileName());
+        }
+
+        public void Scan(out IDocument document, IDocument.FormatType formatType = IDocument.FormatType.PDF)
+        {
+            document = new PDFDocument(filename: null);
+            if (state == IDevice.State.off) return;
+
+            ScanCounter++;
+            if (formatType == IDocument.FormatType.PDF)
+                document = new PDFDocument(filename: " PDFScan" + ScanCounter + ".pdf");
+
+            if (formatType == IDocument.FormatType.JPG)
+                document = new ImageDocument(filename: " ImageScan " + ScanCounter + ".jpg");
+
+            if (formatType == IDocument.FormatType.TXT)
+                document = new TextDocument(filename: " TextScan " + ScanCounter + ".txt");
+
+            Console.WriteLine(DateTime.Now + " Scan:" + document.GetFileName());
+        }
+
+        public void ScanAndPrint()
+        {
+            if (state == IDevice.State.off) return;
+
+            IDocument document;
+            Scan(out document);
+            Print(in document);
         }
     }
 }
--- Zadanie1/Program.cs
+++ Zadanie1/Program.cs
@@ -6,7 +6,18 @@ namespace Zadanie1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var xerox = new Copier();
+            xerox.PowerOn();
+            IDocument doc1 = new PDFDocument("aaa.pdf");
+            xerox.Print(in doc1);
+
+            IDocument doc2;
+            xerox.Scan(out doc2);
+
+            xerox.ScanAndPrint();
+            Console.WriteLine(xerox.Counter);
+            Console.WriteLine(xerox.PrintCounter);
+            Console.WriteLine(xerox.ScanCounter);
         }
     }
 }

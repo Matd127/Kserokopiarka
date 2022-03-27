822f40619b8e4565291c68f3b71bc7f8bb252686
Matd127 <79983536+Matd127@users.noreply.github.com>
2022-03-27 17:38:11 +0200

    Dodaj pliki projektów.

---
+++ Zadanie1/Copier.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie1
+{
+    public class Copier : BaseDevice, IPrinter, IScanner
+    {
+        public void Print(in IDocument document)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Scan(out IDocument document, IDocument.FormatType formatType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
